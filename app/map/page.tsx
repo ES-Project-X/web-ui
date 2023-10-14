e683827f5babbcdeeff9358bf8ef7f636e9dd57b
Gui <guicostaantunes@gmail.com>
2023-10-15 00:37:02 +0100

    map/page restore

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,8 +1,9 @@
 import dynamic from 'next/dynamic'
-import MapComponent from '../components/Map';
 
+const MapComponent = dynamic(() => import('../components/Map'), {
+    ssr: false,
+})
 export default function MapPage() {
-
     return <>
         <MapComponent tileLayerURL={"https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png"}/>
     </>
