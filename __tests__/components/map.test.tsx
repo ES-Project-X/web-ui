ffdf0c0617a5df88657b9b03d8238e11456dba2e
João Fonseca <joao.fonseca@ua.pt>
2023-10-14 13:52:55 +0100

    ES-6 add tests

--- __tests__/components/map.test.tsx
+++ __tests__/components/map.test.tsx
@@ -5,41 +5,79 @@ describe("MapComponent", () => {
 
     let component: HTMLElement
 
-    const findById = (id: string) => {
-        return component.querySelector(`#${id}`) ?? undefined
+    const findById = (container: HTMLElement, id: string) => {
+        return container.querySelector(`#${id}`) ?? undefined
     }
 
-    const findByClass = (className: string) => {
-        return component.querySelector(`.${className}`) ?? undefined
+    const findByClass = (container: HTMLElement, className: string) => {
+        return container.querySelector(`.${className}`) ?? undefined
     }
-
-    beforeEach(() => {
-        const {container} = render(<MapComponent/>)
-        component = container
-    })
 
     it("renders", () => {
-        const mapContainer = findById("map-container")
+        const {container} = render(<MapComponent/>)
+
+        const mapContainer = findById(container, "map-container")
         expect(mapContainer).toBeDefined()
 
-        const rotateRight = findById("map-rotate-right-btn")
+        const rotateRight = findById(container, "map-rotate-right-btn")
         expect(rotateRight).toBeDefined()
 
-        const rotateLeft = findById("map-rotate-left-btn")
+        const rotateLeft = findById(container, "map-rotate-left-btn")
         expect(rotateLeft).toBeDefined()
     })
 
     it("rotates to the right", () => {
-        const rotateRight = findById("map-rotate-right-btn")
+        const {container} = render(<MapComponent/>)
+
+        const rotateRight = findById(container, "map-rotate-right-btn")
         expect(rotateRight).toBeDefined()
 
         fireEvent.click(rotateRight!)
     })
 
     it("rotates to the left", () => {
-        const rotateLeft = findById("map-rotate-left-btn")
+        const {container} = render(<MapComponent/>)
+
+        const rotateLeft = findById(container, "map-rotate-left-btn")
         expect(rotateLeft).toBeDefined()
 
         fireEvent.click(rotateLeft!)
+    })
+
+    it("location is enabled", () => {
+        global.navigator.geolocation = {
+            watchPosition: jest.fn()
+                .mockImplementationOnce((success) => Promise.resolve(success({
+                    coords: {
+                        latitude: 40.64427,
+                        longitude: -8.64554
+                    }
+                })))
+        }
+        const {container} = render(<MapComponent/>)
+
+        const currentLocation = findById(container, "map-user-position")
+        expect(currentLocation).toBeDefined()
+
+        expect(currentLocation!.textContent).toContain("Latitude: 40.64427")
+        expect(currentLocation!.textContent).toContain("Longitude: -8.64554")
+    })
+
+    it("location is not enabled", () => {
+        global.navigator.geolocation = {
+            watchPosition: jest.fn()
+                .mockImplementationOnce((success) => Promise.resolve(success({
+                    coords: {
+                        latitude: undefined,
+                        longitude: undefined
+                    }
+                })))
+        }
+        const {container} = render(<MapComponent/>)
+
+        const currentLocation = findById(container, "map-user-position")
+        expect(currentLocation).toBeDefined()
+
+        expect(currentLocation!.textContent).toContain("Please enable location to see your current location")
     })
 })
