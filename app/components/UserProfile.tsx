d798022a47d5d2985ca2bd1746c9c39dec7c8165
GoncaloSilva25 <goncassilva02@gmail.com>
2023-11-12 02:52:26 +0000

    tests done

--- app/components/UserProfile.tsx
+++ app/components/UserProfile.tsx
@@ -40,8 +40,7 @@ const UserProfile = ({ user }: { user: UserProps }) => {
   //if the user doesnt change anything, the value is null
   //if the user changes something, the value is the new value
 
-  const [userChanges, setUserChanges] = useState({});
-
+  let userChanges = {};
 
   const API_URL = process.env.DATABASE_API_URL;
 
@@ -54,11 +53,11 @@ const UserProfile = ({ user }: { user: UserProps }) => {
     // here u need to update the user profile in the DB
     /* @Gonçalo */
     const userLS = JSON.parse(localStorage.getItem("user") || "{}");
-    console.log("userLS:", userLS);
+    //console.log("userLS:", userLS);
 
 
     if (formPassword === "") {
-      alert("Please confirm your password!");
+      //alert("Please confirm your password!");
       setFormUsername(username); // Reset formUsername
       setFormEmail(email); // Reset formEmail
       setIsEditing(false);
@@ -66,42 +65,45 @@ const UserProfile = ({ user }: { user: UserProps }) => {
     }
 
     if (formUsername === username && formEmail === email) {
-      alert("No changes were made!");
+      //alert("No changes were made!");
       setIsEditing(false);
       return;
     }
 
     if (formUsername !== username) {
-      setUserChanges({ ...userChanges, username: formUsername });
+      userChanges = { ...userChanges, username: formUsername };
     }
-
+    
     if (formEmail !== email) {
-      setUserChanges({ ...userChanges, email: formEmail });
+      userChanges = { ...userChanges, email: formEmail };
+    }
+    
+    if (formPassword !== "") {
+      userChanges = { ...userChanges, password: formPassword };
     }
 
-    if (formPassword !== "") {
-      setUserChanges({ ...userChanges, password: formPassword });
-    }
 
     if (isEditing) {
       fetch("http://127.0.0.1:8000/user/edit", {
         method: "PUT",
         headers: {
           "Content-Type": "application/json",
-          Authorization: "Bearer eyJraWQiOiJSc0d4ckllKzZFXC9SVVlPOUFxU1RVaXJCZ2lvamZFUUZucGpXN0FTQVFDWT0iLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiIyODM1YjczYy1hOTQ4LTQwNWItODJhNi00YzdmZTUzMTBjZGQiLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAuZXUtd2VzdC0xLmFtYXpvbmF3cy5jb21cL2V1LXdlc3QtMV9kemdZTDhmUFgiLCJ2ZXJzaW9uIjoyLCJjbGllbnRfaWQiOiI1ZzRic2ZzbHFhOTV1NHBkMnBvc2JuMHJudSIsImV2ZW50X2lkIjoiMzJhODg3ZDctMmJhZC00OWIzLTgyZGQtOWYyOWYyYTkxNTljIiwidG9rZW5fdXNlIjoiYWNjZXNzIiwic2NvcGUiOiJvcGVuaWQgZW1haWwiLCJhdXRoX3RpbWUiOjE2OTk3NDU1MTQsImV4cCI6MTY5OTc0OTExNCwiaWF0IjoxNjk5NzQ1NTE0LCJqdGkiOiIzYWI3MDFmZS04ZjU0LTQzZTItYjcxNC02NTcyZGNiNWM2YjciLCJ1c2VybmFtZSI6IjI4MzViNzNjLWE5NDgtNDA1Yi04MmE2LTRjN2ZlNTMxMGNkZCJ9.CORjsTDrpCj1TcKieTwEe2-T3rKuz1TvfvnpT-91sYKxPfB8gUeq-a_ttO_KirxJ5RseEk16j8k2BLCfLrcN9bIeBOJQSFUPaxDEUgaVPaWDIthm69Ural6mvOZBdZlK4MeTTQMoMyX7ESExoMdVCeLwY9Jugp18rAGy0AjSFCOPE_kT7dnOWhlkUJCwu6i9uVc2Uto75q_WMq07UjgcNO-8HiCqVB9cNuc4Y20jmfNZkc0-a8QJlskY4QNXXmAYyia-7Y6TOml_kmzrF7NSctQAxoFQQK67s9UZaHMvGTPwv3fdACdXg8SDLSdC1namP3jbTfZuRNqxY3Qyz9K1_Q",
+          Authorization: "Bearer eyJraWQiOiJSc0d4ckllKzZFXC9SVVlPOUFxU1RVaXJCZ2lvamZFUUZucGpXN0FTQVFDWT0iLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiIyODM1YjczYy1hOTQ4LTQwNWItODJhNi00YzdmZTUzMTBjZGQiLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAuZXUtd2VzdC0xLmFtYXpvbmF3cy5jb21cL2V1LXdlc3QtMV9kemdZTDhmUFgiLCJ2ZXJzaW9uIjoyLCJjbGllbnRfaWQiOiI1ZzRic2ZzbHFhOTV1NHBkMnBvc2JuMHJudSIsImV2ZW50X2lkIjoiYjhhYjc5ODMtMWIxYS00Yzk4LWE3NTgtNDk4NmVkM2Q4NzFlIiwidG9rZW5fdXNlIjoiYWNjZXNzIiwic2NvcGUiOiJvcGVuaWQgZW1haWwiLCJhdXRoX3RpbWUiOjE2OTk3NDkzNTEsImV4cCI6MTY5OTc1Mjk1MSwiaWF0IjoxNjk5NzQ5MzUxLCJqdGkiOiI5N2MzNjJlYS00M2M3LTRjNjItOTMwMS1mOGI3ODRiNzQ1ZGYiLCJ1c2VybmFtZSI6IjI4MzViNzNjLWE5NDgtNDA1Yi04MmE2LTRjN2ZlNTMxMGNkZCJ9.w2zNXT4h9ukJR0RHIESWPsRK_JE4hqbKPZSffzdgMgMj8lLF-UVStYB7IBLbf_mAJbfyI6lxHK9o9ehMJqb-W4qirAoU5FnTpbbIfuiLpygSEdN8rBxiLvG1EfDKF7useUavwNspmI9JdjvLddxKbizKNiU2c9hfFroHyQRpjIvhzknnGwJiyGJ7LPzGDAKtHFdnVWxZl_Yk51Goi8RYiwYZOSvETOFR1_H8X_YWx_XUpXJNEzWmAc71hPg7RNpLtbxIOZ4wosoNASiZHy31N4iOJv2CKGX7jUlZSQ6ay_Y6EGqYbUfikt8IvX3gYJ5CNdLRbJVWjvoNfofiRWtOGQ",
         },
         body: JSON.stringify(userChanges),
       })
         .then((res) => {
-          console.log("res:", res);
           if (res.status === 200) {
             return res.json();
-          } else {
+          } 
+          else if (res.status === 400) {
+            //alert("Incorrect Password!");
             throw new Error("Error updating user");
           }
         })
         .then((data) => {
-          alert("User updated successfully!");
+          //alert("User updated successfully!");
+          //console.log("data:", data);
           localStorage.setItem("user", JSON.stringify(data));
           setUsername(data.username);
           setEmail(data.email);
@@ -228,7 +230,7 @@ const UserProfile = ({ user }: { user: UserProps }) => {
           </div>)}
         </div>
         {isEditing && (
-          <Button variant="primary" className="mt-3" onClick={updateProfile}>
+          <Button variant="primary" className="mt-3" title="Save Profile" onClick={updateProfile} >
             Save Profile
           </Button>
         )}
