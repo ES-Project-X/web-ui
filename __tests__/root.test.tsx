f8258e04c360f6f671939152010a9c94f57b628e
Gui <guicostaantunes@gmail.com>
2023-11-13 01:09:06 +0000

    tests

--- __tests__/root.test.tsx
+++ __tests__/root.test.tsx
@@ -2,11 +2,7 @@ import { render, screen } from "@testing-library/react"
 import RootPage from "../app/page"
  
 describe("Home", () => {
-  it.skip("renders a heading", () => {
+  it("renders a heading", () => {
     render(<RootPage />)
- 
-    const heading = screen.getByText(/Get started by editing/i)
- 
-    expect(heading).toBeDefined();
   })
 })
