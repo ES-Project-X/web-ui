386636c511ef3e4d3272521e2b9927816f2f6566
r4sinhas <rasinhas@ua.pt>
2023-12-15 00:45:54 +0000

    remove duplicate svg

--- app/components/POIsSidebar.tsx
+++ app/components/POIsSidebar.tsx
@@ -233,24 +233,28 @@ const POIsSidebar = ({
     }
   }, [selectedTimePeriod, fullData]);
 
+  const duplicatedSVG = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      className="w-6 h-6"
+    >
+      <path
+        fillRule="evenodd"
+        d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-1.72 6.97a.75.75 0 10-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 101.06 1.06L12 13.06l1.72 1.72a.75.75 0 101.06-1.06L13.06 12l1.72-1.72a.75.75 0 10-1.06-1.06L12 10.94l-1.72-1.72z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+
   const displayMainContent = (
     <div className="main-content" style={{ width: 250 }}>
       <div className="sidebar-body">
         <div className="sidebar-body-item">
           <div className={"flex justify-end pb-2"}>
             <button className="close-button" onClick={hideCard}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-1.72 6.97a.75.75 0 10-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 101.06 1.06L12 13.06l1.72 1.72a.75.75 0 101.06-1.06L13.06 12l1.72-1.72a.75.75 0 10-1.06-1.06L12 10.94l-1.72-1.72z"
-                  clipRule="evenodd"
-                />
-              </svg>
+              {duplicatedSVG}
             </button>
           </div>
           <div
@@ -416,18 +420,7 @@ const POIsSidebar = ({
             data-testid="close-button"
             onClick={hideCard}
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="w-6 h-6"
-            >
-              <path
-                fillRule="evenodd"
-                d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zm-1.72 6.97a.75.75 0 10-1.06 1.06L10.94 12l-1.72 1.72a.75.75 0 101.06 1.06L12 13.06l1.72 1.72a.75.75 0 101.06-1.06L13.06 12l1.72-1.72a.75.75 0 10-1.06-1.06L12 10.94l-1.72-1.72z"
-                clipRule="evenodd"
-              />
-            </svg>
+            {duplicatedSVG}
           </button>
         </div>
         <div
